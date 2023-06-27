f626605a212b4929a95f4b54b1d67854b14e4227
ThienViet <thienbaodoanle@gmail.com>
2023-06-27 04:26:15 +0200

    It only needs one file.
    It took countless of hours to make this one.

--- manager.py
+++ manager.py
@@ -1,57 +1,83 @@
 import os
 import json
-passwords = {}
 
 # if file exist, return True or else False
 
 with open('.passwords/passwords.key', 'r') as f:
-    password_data = f.read()
-    password_dict = json.loads(password_data.replace("'", '"'))
-    del password_dict["google.com"]
-    alias = input('Alias: ')
-    username = input('Username: ')
-    password = input('Password: ')
-    password_dict[alias] = {"Username": username, "Password": password}
-    alias = input('Alias: ')
-    username = input('Username: ')
-    password = input('Password: ')
-    password_dict[alias] = {"Username": username, "Password": password}
+    try:
+        password_data = f.read()
+        password_dict = json.loads(password_data.replace("'", '"'))
+    except json.decoder.JSONDecodeError:
+        list = {}
+    # alias = input('Alias: ')
+    # username = input('Username: ')
+    # password = input('Password: ')
+    # password_dict[alias] = {"Username": username, "Password": password}
+    # with open('.passwords/passwords.key', 'w') as f:
+    #     f.write(str(password_dict))
+    # print("------------------------------------------")
+    # print("Password is successfully added/updated!")
+    # print()
     list = list(password_dict.keys())
+    print(list)
     for l in list:
         print(l)
 
-# def exist(a):
-#     f'.passwords/passwords.key'
-#     with open('.passwords/passwords.key', 'r') as f:
-#         print(f.read())
-#     if os.path.exists(f'.passwords/passwords.key'):
-#         return True
-#     else:
-#         return False
+# alias = input("Alias: ")
 
-# # add folder
+def exist(a):
+    with open('.passwords/passwords.key', 'r') as f:
+        list = []
+        password_data = f.read()
+        password_dict = json.loads(password_data.replace("'", '"'))
+        dict = password_dict.keys()
+        for l in dict:
+            list.append(l)
+    if a in list:
+        return True
+    else:
+        return False
 
-# def add_folder():
-#     path = '.passwords'
-#     if os.path.exists(path):
-#         return True
-#     else:
-#         return False
+# add folder
 
-# # add passwords
+def add_folder():
+    path = '.passwords'
+    if os.path.exists(path):
+        return True
+    else:
+        return False
 
-# def add_password():
-#     print("------------------------------------------")
-#     print("If you want to stop this process, then you can press 'controll^ + z' (Mac)")
-#     alias = str(input("Alias: "))
-#     username = str(input("Username: "))
-#     password = str(input("Password: "))
-#     passwords[alias] = {alias:{"Username": username, "Password": password}}
-#     with open(f'.passwords/passwords.key', 'w') as f:
-#         f.write(str(passwords[alias]))
-#     print("------------------------------------------")
-#     print("Password is successfully added/updated!")
-#     print()
+# add passwords
+
+def add_password():
+    print("------------------------------------------")
+    print("If you want to stop this process, then you can press 'controll^ + z' (Mac)")
+    with open('.passwords/passwords.key', 'r') as f:
+        try:
+            password_data = f.read()
+            password_dict = json.loads(password_data.replace("'", '"'))
+        except json.decoder.JSONDecodeError:
+            print('But because there is nothing in the file you can create one safetly.')
+    alias = input('Alias: ')
+    username = input('Username: ')
+    password = input('Password: ')
+    password_dict[alias] = {"Username": username, "Password": password}
+    if exist(alias) == True:
+        copy = input("There is an alias with the same name. It will be updated. \nAre you sure you want to update it: [y/n] ")
+        print("------------------------------------------")
+        if copy == 'y':
+            with open('.passwords/passwords.key', 'w') as f:
+                f.write(str(password_dict))
+            answer = "Password is successfully updated!"
+        elif copy == 'n':
+            answer = "That might be a good choice."
+        else:
+            answer = 'Thst is an invalid choice. No password will be change.'
+    else:
+        answer = "Password is successfully added!"
+    print("------------------------------------------")
+    print(answer)
+    print()
 
 # def get_password():
 #         alias = input("Alias: ")
@@ -120,45 +146,28 @@ with open('.passwords/passwords.key', 'r') as f:
 
 # # combine everything
 
-<<<<<<< Updated upstream
+while True:
+    if add_folder() == False:
+        os.makedirs('.passwords')
+    print()
+    print("1. Add/Update Alias")
+    print("2. Get Password From Alias")
+    print("3. Show All Aliases")
+    print("4. Remove Alias")
+    print("5. Quit")
+    choice = input("Enter your choice: ")
+
     if choice == "1":
         add_password()
-    elif choice == "2":
-        get_password()
-    elif choice == "3":
-        all_password()
-    elif choice == "4":
-        del_password()
+    # elif choice == "2":
+    #     get_password()
+    # elif choice == "3":
+    #     all_password()
+    # elif choice == "4":
+    #     del_password()
     elif choice == "5":
         break
     else:
         print("------------------------------------------")
         print("Invalid choice. Please try again.")
         print()
-=======
-# while True:
-#     if add_folder() == False:
-#         os.makedirs('.passwords')
-#     print()
-#     print("1. Add/Update Alias")
-#     print("2. Get Password From Alias")
-#     print("3. Show All Aliases")
-#     print("4. Remove Alias")
-#     print("5. Quit")
-#     choice = input("Enter your choice: ")
-
-#     if choice == "1":
-#         add_password()
-#     elif choice == "2":
-#         get_password()
-#     elif choice == "3":
-#         all_password()
-#     elif choice == "4":
-#         del_password()
-#     elif choice == "5":
-#         break
-#     else:
-#         print("------------------------------------------")
-#         print("Invalid choice. Please try again.")
-#         print()
->>>>>>> Stashed changes
