153d5682ffd6e2835c97bdab6c7b9faf3f1a358d
ThienViet <thienbaodoanle@gmail.com>
2023-06-27 02:00:35 +0200

    Ideas
    I am writing ideas.

--- manager.py
+++ manager.py
@@ -4,39 +4,56 @@ passwords = {}
 
 # if file exist, return True or else False
 
-def exist(alias):
-    f'.passwords/{alias}.key'
-    if os.path.exists(f'.passwords/{alias}.key'):
-        return True
-    else:
-        return False
+with open('.passwords/passwords.key', 'r') as f:
+    password_data = f.read()
+    password_dict = json.loads(password_data.replace("'", '"'))
+    del password_dict["google.com"]
+    alias = input('Alias: ')
+    username = input('Username: ')
+    password = input('Password: ')
+    password_dict[alias] = {"Username": username, "Password": password}
+    alias = input('Alias: ')
+    username = input('Username: ')
+    password = input('Password: ')
+    password_dict[alias] = {"Username": username, "Password": password}
+    list = list(password_dict.keys())
+    for l in list:
+        print(l)
 
-# add folder
+# def exist(a):
+#     f'.passwords/passwords.key'
+#     with open('.passwords/passwords.key', 'r') as f:
+#         print(f.read())
+#     if os.path.exists(f'.passwords/passwords.key'):
+#         return True
+#     else:
+#         return False
 
-def add_folder():
-    path = '.passwords'
-    if os.path.exists(path):
-        return True
-    else:
-        return False
+# # add folder
 
-# add passwords
+# def add_folder():
+#     path = '.passwords'
+#     if os.path.exists(path):
+#         return True
+#     else:
+#         return False
 
-def add_password():
-    print("------------------------------------------")
-    print("If you want to stop this process, then you can press 'controll^ + z' (Mac)")
-    alias = str(input("Alias: "))
-    username = str(input("Username: "))
-    password = str(input("Password: "))
-    passwords[alias] = {alias:{"Username": username, "Password": password}}
-    with open(f'.passwords/{alias}.key', 'w') as f:
-        f.write(str(passwords[alias]))
-    print("------------------------------------------")
-    print("Password is successfully added/updated!")
-    print()
+# # add passwords
 
-# get passwords
+# def add_password():
+#     print("------------------------------------------")
+#     print("If you want to stop this process, then you can press 'controll^ + z' (Mac)")
+#     alias = str(input("Alias: "))
+#     username = str(input("Username: "))
+#     password = str(input("Password: "))
+#     passwords[alias] = {alias:{"Username": username, "Password": password}}
+#     with open(f'.passwords/passwords.key', 'w') as f:
+#         f.write(str(passwords[alias]))
+#     print("------------------------------------------")
+#     print("Password is successfully added/updated!")
+#     print()
 
+<<<<<<< Updated upstream
 def get_password():
         alias = input("Alias: ")
         print("------------------------------------------")
@@ -55,65 +72,78 @@ def get_password():
         except KeyError:
             print("Yeah, please remember that I wrote 'that most operating systems are case-intensitive.'")
         print()
+=======
+# # get passwords
+>>>>>>> Stashed changes
 
-# show all aliases
+# def get_password():
+#         alias = input("Alias: ")
+#         print("------------------------------------------")
+#         try:
+#             if exist(alias):
+#                 with open('.passwords/passwords.key', 'r') as f:
+#                     password_data = f.read()
+#                     password_dict = json.loads(password_data.replace("'", '"'))
+#                     print(password_dict)
+#                     # password_dict = password_dict[alias]
+#                     # username = password_dict["Username"]
+#                     # password = password_dict["Password"]
+#                     # print(f"Username: {username}")
+#                     # print(f"Password: {password}")
+#             else:
+#                 print('Are you sure that it really exist?')
+#         except KeyError:
+#             print("Yeah, please remembder that I wrote 'that most operating systems are case-intensitive.'")
+#         print()
 
-def all_password():
-    print("------------------------------------------")
-    files = []
-    for file in os.listdir('.passwords'):
-        x = file.split(".")
-        le = len(x)
-        del x[le-1]
-        files = '.'.join(x)
-        print(files)
-    print()
+# # show all aliases
 
-# delete alias
+# def all_password():
+#     print("------------------------------------------")
+#     files = []
+#     for file in os.listdir('.passwords'):
+#         x = file.split(".")
+#         le = len(x)
+#         del x[le-1]
+#         files = '.'.join(x)
+#         print(files)
+#     print()
 
-def del_password():
-    alias = str(input("Alias: "))
-    print("------------------------------------------")
-    if exist(alias):
-        with open(f'.passwords/{alias}.key', 'r') as f:
-            password_data = f.read()
-            password_dict = json.loads(password_data.replace("'", '"'))
-            password_dict = password_dict[alias]
-            username = password_dict["Username"]
-            password = password_dict["Password"]
-            print(f"Username: {username}")
-            print(f"Password: {password}")
-            print()
-            thinking = input("Are you sure you want to delete it: [y/n] ")
-            print("------------------------------------------")
-            if thinking == "y":
-                os.remove(f'.passwords/{alias}.key')
-                if os.path.exists(alias):
-                    print("Please, delete it manually.")
-                else:
-                    print(f"Alias '{alias}' has been deleted.")
-            elif thinking == "n":
-                print('That might be a good choice.')
-            else:
-                print("You wrote something wrong. IDK, what you were tiping, but it counts as a 'no' anyway.")
-            print()
-    else:
-        print('Since when was this alias created?')
-        print()
+# # delete alias
 
-# combine everything
+# def del_password():
+#     alias = str(input("Alias: "))
+#     print("------------------------------------------")
+#     if exist(alias):
+#         with open(f'.passwords/passwords.key', 'r') as f:
+#             password_data = f.read()
+#             password_dict = json.loads(password_data.replace("'", '"'))
+#             password_dict = password_dict[alias]
+#             username = password_dict["Username"]
+#             password = password_dict["Password"]
+#             print(f"Username: {username}")
+#             print(f"Password: {password}")
+#             print()
+#             thinking = input("Are you sure you want to delete it: [y/n] ")
+#             print("------------------------------------------")
+#             if thinking == "y":
+#                 os.remove(f'.passwords/{alias}.key')
+#                 if os.path.exists(alias):
+#                     print("Please, delete it manually.")
+#                 else:
+#                     print(f"Alias '{alias}' has been deleted.")
+#             elif thinking == "n":
+#                 print('That might be a good choice.')
+#             else:
+#                 print("You wrote something wrong. IDK, what you were tiping, but it counts as a 'no' anyway.")
+#             print()
+#     else:
+#         print('Since when was this alias created?')
+#         print()
 
-while True:
-    if add_folder() == False:
-        os.makedirs('.passwords')
-    print()
-    print("1. Add/Update Alias")
-    print("2. Get Password From Alias")
-    print("3. Show All Aliases")
-    print("4. Remove Alias")
-    print("5. Quit")
-    choice = input("Enter your choice: ")
+# # combine everything
 
+<<<<<<< Updated upstream
     if choice == "1":
         add_password()
     elif choice == "2":
@@ -128,3 +158,30 @@ while True:
         print("------------------------------------------")
         print("Invalid choice. Please try again.")
         print()
+=======
+# while True:
+#     if add_folder() == False:
+#         os.makedirs('.passwords')
+#     print()
+#     print("1. Add/Update Alias")
+#     print("2. Get Password From Alias")
+#     print("3. Show All Aliases")
+#     print("4. Remove Alias")
+#     print("5. Quit")
+#     choice = input("Enter your choice: ")
+
+#     if choice == "1":
+#         add_password()
+#     elif choice == "2":
+#         get_password()
+#     elif choice == "3":
+#         all_password()
+#     elif choice == "4":
+#         del_password()
+#     elif choice == "5":
+#         break
+#     else:
+#         print("------------------------------------------")
+#         print("Invalid choice. Please try again.")
+#         print()
+>>>>>>> Stashed changes
